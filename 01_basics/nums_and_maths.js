02a0220bafcc2b5c4528a81be364db42e0edacca
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-08 16:34:58 +0000

    nums_and_maths

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,35 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN')); //1,00,00,000
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.8)); //5
+// console.log(Math.min(3,5,6,7,8,1,8));
+// console.log(Math.max(45,43,23,12,67));
+
+
+console.log(Math.random()); 
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20 
+
+console.log((Math.floor(Math.random()) * (max - min + 1)) + min);
