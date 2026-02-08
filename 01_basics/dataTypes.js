a0b4cc2d1ba45cf74670c78067983b51ba4bd9e1
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-08 03:23:00 +0000

    added dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,34 @@
+"use strict "; //treat all js code as newer version
+
+//alert(3+4) //we are using nodejs ,not browser
+
+console.log (3
+     +
+      3) // code readability should be high
+
+console.log("john")
+
+/////DATATYPES
+
+let name = "john" //string datatype
+let age = 23  //int(number) datatype
+let IsLoggedIn = false //boolean datatype
+let state = null
+
+//numvber =>2 to power 53
+//bigint 
+
+//string => ""
+
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol =>unique
+
+
+//object
+
+console.log(typeof"john");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
