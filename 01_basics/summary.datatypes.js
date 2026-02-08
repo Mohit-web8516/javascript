1bb3381ade55fb3c750e55166266dcf2ccb2c0f4
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-08 15:22:48 +0000

    summary.datatyps

--- 01_basics/summary.datatypes.js
+++ 01_basics/summary.datatypes.js
@@ -9,32 +9,54 @@
 // const outsideTemp = null 
 // let userEmail;
 
-const id = Symbol ('123')
-const anotherId = Symbol ('123')
+// const id = Symbol ('123')
+// const anotherId = Symbol ('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-// const bigNumber = 365465645648767546785n
-
-
+// // const bigNumber = 365465645648767546785n
 
 
 
 
 
-// Reference (Non primitive datatypes)
 
-//Array , objects , functions.
 
-const heros = ["shaktiman","naagraj", "doga"]
-let myObj = {
-    name : "john",
-    age: 22,
+// // Reference (Non primitive datatypes)
+
+// //Array , objects , functions.
+
+// const heros = ["shaktiman","naagraj", "doga"]
+// let myObj = {
+//     name : "john",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log ("hello world");
+// }
+// console.log(typeof bigNumber); ///undefined
+// console.log(typeof myFunction); ///function
+// console.log(typeof anotherId); ///symbol
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// STACK (Primitive ),Heap (non-primitive)
+
+let myYoutubename = "johnrobertdotcom"
+
+let anothername = myYoutubename
+anothername = "therobertlife"
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
 }
 
-const myFunction = function(){
-    console.log ("hello world");
-}
-console.log(typeof bigNumber); ///undefined
-console.log(typeof myFunction); ///function
-console.log(typeof anotherId); ///symbol
+let userTwo = userOne
+
+userTwo.email = "john@google.com"
