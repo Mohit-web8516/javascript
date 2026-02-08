503eaffb2f8c1270fde46b1ce3c1d63f280747d4
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-08 17:02:06 +0000

    date-and-time

---
+++ 01_basics/date.js
@@ -0,0 +1,35 @@
+// let d = new Date();
+
+// console.log(d.getFullYear());   // year (e.g., 2026)
+// console.log(d.getMonth());      // month (0–11)
+// console.log(d.getDate());       // day of month (1–31)
+// console.log(d.getDay());        // day of week (0–6, Sunday=0)
+// console.log(d.getHours());      // hour (0–23)
+// console.log(d.getMinutes());    // minutes (0–59)
+// console.log(d.getSeconds());    // seconds (0–59)
+// console.log(d.getMilliseconds());// milliseconds (0–999)
+// console.log(d.toString());
+// console.log(d.toLocaleString()); 
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// 
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// newDate.toLocaleDateString('default', {
+//     weekday: "long",
+// })
