b7faff2cc2d929f27718bf6ece49395f5833e054
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-08 04:27:43 +0000

    operations

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,27 +1,69 @@
-let score = 23
+// let score = 23
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //NaN
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); //NaN
 // "23" => 23
 // "23ABC" => NaN
 // true => 1; false => 0;
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean((isLoggedIn))
+// let booleanIsLoggedIn = Boolean((isLoggedIn))
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 //"hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************* Operations ********************** 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " alex"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + 2); //3
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2");  //32
+// console.log((3 + 4) * 5 % 3);  //2
+
+// console.log(+true); //1
+// //console.log(true+); //error
+// console.log(+"");
+
+// let num1 ,num2 ,num3 
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// gameCounter++;     //postfix
+// console.log(gameCounter); //101
+
+
+
+
+
