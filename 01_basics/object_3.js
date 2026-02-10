b35296585dbc1015d00e9ada83b0f232544d5979
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-10 14:41:50 +0000

    json

---
+++ 01_basics/object_3.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename: "js in hindi",
+    price : "999",
+    courseInstructor: "alex"
+}
+
+//course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "john",
+//     "course": "js in hindi",
+//     "price" : "free"
+
+// }
+
+[
+    {},
+    {},
+    {}
+]
