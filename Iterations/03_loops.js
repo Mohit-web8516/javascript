6a89cb4445207efe966ce0d3cf61b89db415f5ce
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-11 17:28:21 +0000

    loops_03

---
+++ Iterations/03_loops.js
@@ -0,0 +1,51 @@
+// for of 
+
+//["","",""]
+// [{}, {}, {}]
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7]
+
+// for (const num of arr){
+//     console.log(num);
+
+// }
+
+//////////////////////EXAMPLE////////////////////
+// const numbers = [10, 20, 30];
+
+// for (let num of numbers) {
+//   console.log(num);
+// }
+////////////////////////////EXAMPLE/////////////////
+
+// const greetings = "Hello world!"
+// for (const greet of greetings ){
+//     console.log(`Each char is ${greet}`)
+// }
+
+/////////////////////////////////////////////////
+//MAPS
+// const map = new Map()
+// map.set ('IN', "India")
+// map.set('USA',"UNITED STATES OF AMERICA")
+// map.set('Fr',"France")
+// console.log(map); //gives unique values
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+//FOR IN LOOP
+
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'c++',
+//     rb : 'ruby',
+//     swift: "swift by apple"
+// }
+// for (const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+
