b8c76f8fbbbae040551034c80006d91643f9988e
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-11 15:22:56 +0000

    break

---
+++ control_flow/switch.js
@@ -0,0 +1,56 @@
+/////SYNTAX
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch(month){
+//     case 1:
+//         console.log("Janaury");
+//         break;
+//     case 2:
+//         console.log("february")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+///////////////////////////////////////////////////
+// const month = "april"
+// switch(month){
+//     case "janaury":
+//         console.log("Janaury");
+//         break;
+//     case "february":
+//         console.log("february")
+//         break;
+//     case "march":
+//         console.log("march")
+//         break;
+//     case "april":
+//         console.log("april")
+//         break;
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+
+
+///////////////////++++++++++++++++++++++++++++++++++++
+///////////////////++++++++++++++++++++++++++++++++++++
+///////////////////++++++++++++++++++++++++++++++++++++
+
