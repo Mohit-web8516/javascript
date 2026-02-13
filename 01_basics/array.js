8a59331e24aed3f3c933c5cad463ae2a79343036
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-13 13:23:26 +0000

    array

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -64,8 +64,8 @@
 // console.log(Array.from({name: "AlexRobert"}))
 
 ////////////////////////////////
-// let score1 = 100
-// let score2 = 200
-// let score3 = 300
+let score1 = 100
+let score2 = 200
+let score3 = 300
 
-// console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));
