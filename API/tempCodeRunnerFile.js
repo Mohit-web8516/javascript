531998fc1bc972d332b8a8f1abc038b019faaaf1
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-19 18:11:18 +0000

    update

---
+++ API/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+ync function getAllUsers(){
+//     try {
+//         const response = await fetch('https://github.com/Mohit-web8516')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+// getAllUsers()
