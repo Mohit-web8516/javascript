453f4a2f903b4bf5f63584eb7fd949876854e340
Mohit Shrivastav <mohitshrivastav429@gmail.com>
2026-02-19 18:57:17 +0000

    UPDATE

--- Oops/tempCodeRunnerFile.js
+++ Oops/tempCodeRunnerFile.js
@@ -1,5 +1,12 @@
+const john = new User("john")
+// console.log(john.createId())
 
-// heroPower.john()
-// myHeros.john()
-// myHeros.Heyjohn()
-// heroPower.Heyjohn()
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
